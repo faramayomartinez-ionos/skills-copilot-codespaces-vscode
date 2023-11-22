bcbd1eb8c200f99449a7314809954b026cddf753
Francisco Aramayo <147380230+faramayomartinez-ionos@users.noreply.github.com>
2023-11-22 12:51:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,3 @@
 // create web server
-// run node comments.js
-// and open http://localhost:3000/comments.html
-
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
-
-// cache object
-var cache = {};
-
-// send 404 error
+// 3000 port
+//
