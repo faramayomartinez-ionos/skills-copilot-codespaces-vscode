724d4892ffee152453cb2be5a808b0918c66d37c
Francisco Aramayo <147380230+faramayomartinez-ionos@users.noreply.github.com>
2023-11-22 12:56:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,17 @@
 // create web server
-// 3000 port
-//
+// express is a function
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const { Comment } = require('../models/comment');
+const { User } = require('../models/user');
+
+// path: /api/comments
+// get all comments
+router.get('/', async (req, res) => {
+    const comments = await Comment.find().sort('date');
+    res.send(comments);
+});
+
+// path: /api/comments/:id
+// get comment by id
